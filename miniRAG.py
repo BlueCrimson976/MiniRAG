4fd50943cb59cbf0ceb8a1fd8842d959eddedcaa
RishitaRay <78993352+BlueCrimson976@users.noreply.github.com>
2025-03-07 19:13:15 +0530

    Update miniRAG.py

--- miniRAG.py
+++ miniRAG.py
@@ -8,7 +8,7 @@ from langchain_core.runnables import chain
 from langchain.prompts import ChatPromptTemplate, PromptTemplate 
 from langchain_core.output_parsers import StrOutputParser , JsonOutputParser 
 from langchain_core.runnables import RunnablePassthrough
-from langchain_community.vectorstores import Chroma
+from langchain_chroma import Chroma
 from langchain.schema import Document
 from langchain_huggingface.embeddings import HuggingFaceEmbeddings
 from sentence_transformers import SentenceTransformer
